35335e0e9bf7c440112c4468997ecf2dbf019f6d
Russell Johnson <russjohnson09@gmail.com>
2026-07-29 01:51:42 -0400

    repeating clouds

---
+++ backgrounds/animated_storm_eagle_clouds.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+
+
+# Clouds are 256 x 256.
+
+# Clamp Loop Interpolation will set the animation back to zero on the loop.
+# So I must move to a multiple of 256.
+
+# 512 down and 256 to the left would work for example.
+
+
+# Can I combine these into a single texture?
+
+#https://www.reddit.com/r/godot/comments/qefyzo/how_combine_multiple_transformed_sprites_into_one/
+#If you really really need to make the sprite in parts, you might want to put them in an atlas together so they can be batched.
+
+
+# Single Atlas Texture
+
+
+# Animated Texture ? Waterfalls and simple animations it looks like.
+
+
+
+
+func _ready() -> void:
+	$AnimationPlayer.speed_scale = 1.6
+
+	$AnimationPlayer.play('new_animation')
---
+++ tests/moving_background_clouds.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+
+#https://docs.godotengine.org/en/4.6/tutorials/2d/2d_transforms.html
