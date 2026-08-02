2a3bba93903a73342f67ce6575fe647e2f92d35b
Russell Johnson <russjohnson09@gmail.com>
2026-08-02 11:41:16 -0400

    debugs

--- tests/mega_man_2_parallax.gd
+++ tests/mega_man_2_parallax.gd
@@ -3,6 +3,10 @@ extends Node2D
 
 
 
+func _ready() -> void:
+	
+	DebugMenu.style = DebugMenu.Style.VISIBLE_DETAILED
+
 
 func _process(delta: float) -> void:
 	$Path2D/PathFollow2D.progress_ratio += (delta / 10)
