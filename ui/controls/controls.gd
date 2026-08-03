5f5d56b611ef951f6b08c5b95f30c6b3246e0576
Russell Johnson <russjohnson09@gmail.com>
2026-08-03 00:49:59 -0400

    move controller ui and simulate player input

---
+++ ui/controls/controls.gd
@@ -0,0 +1,52 @@
+extends CanvasLayer
+
+
+@export var input_prefix = "p1_"
+
+
+
+
+
+func _on_right_btn_pressed() -> void:
+	
+	Input.action_press(input_prefix + "right")
+	pass # Replace with function body.
+
+
+func _on_right_btn_released() -> void:
+	Input.action_release(input_prefix + "right")
+	pass # Replace with function body.
+
+
+func _on_left_btn_pressed() -> void:
+	Input.action_press(input_prefix + "left")
+	pass # Replace with function body.
+
+
+func _on_left_btn_released() -> void:
+	Input.action_release(input_prefix + "left")
+	pass # Replace with function body.
+	
+
+
+func _on_up_btn_pressed() -> void:
+	Input.action_press(input_prefix + "jump")
+
+	pass # Replace with function body.
+
+
+func _on_up_btn_released() -> void:
+	Input.action_release(input_prefix + "jump")
+
+	pass # Replace with function body.
+
+
+func show_hide_debug():
+	
+	if 	DebugMenu.style == DebugMenu.Style.VISIBLE_DETAILED:
+		DebugMenu.style = DebugMenu.Style.HIDDEN
+	else:
+		DebugMenu.style = DebugMenu.Style.VISIBLE_DETAILED
+
+func _on_debug_btn_pressed() -> void:
+	show_hide_debug()
